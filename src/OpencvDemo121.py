ec3c414be75c679625b4dd0eccbab0a31d26fb88
wuzm <wuzm@infore.com>
2022-04-12 17:04:17 +0800

    OpencvDemo121() appended.

---
+++ src/OpencvDemo121.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#coding = utf-8
+
+import logging
+import cv2 as cv
+
+bin_model = "models/googlenet/bvlc_googlenet.caffemodel"
+protxt = "models/googlenet/bvlc_googlenet.prototxt"
+
+# OpenCV DNN 获取导入模型各层信息
+def OpencvDemo121():
+    logging.basicConfig(level=logging.DEBUG)
+
+    # load CNN model
+    net = cv.dnn.readNet(bin_model, protxt)
+
+    logging.info("Successfully loaded a model.")
+
+    # get info of layers
+    layer_names = net.getLayerNames()
+    for name in layer_names:
+        id = net.getLayerId(name)
+        layer = net.getLayer(id)
+        logging.info("layer id: {:d}, layer type: {}, layer name: {}".format(id, layer.type, layer.name))
+    
+    return cv.Error.StsOk
+
+if __name__ == "__main__":
+    OpencvDemo121()
+
+# end of file
--- src/main.py
+++ src/main.py
@@ -122,6 +122,26 @@ from OpencvDemo117 import OpencvDemo117
 from OpencvDemo118 import OpencvDemo118
 from OpencvDemo119 import OpencvDemo119
 from OpencvDemo120 import OpencvDemo120
+from OpencvDemo121 import OpencvDemo121
+# from OpencvDemo122 import OpencvDemo122
+# from OpencvDemo123 import OpencvDemo123
+# from OpencvDemo124 import OpencvDemo124
+# from OpencvDemo125 import OpencvDemo125
+# from OpencvDemo126 import OpencvDemo126
+# from OpencvDemo127 import OpencvDemo127
+# from OpencvDemo128 import OpencvDemo128
+# from OpencvDemo129 import OpencvDemo129
+# from OpencvDemo130 import OpencvDemo130
+# from OpencvDemo131 import OpencvDemo131
+# from OpencvDemo132 import OpencvDemo132
+# from OpencvDemo133 import OpencvDemo133
+# from OpencvDemo134 import OpencvDemo134
+# from OpencvDemo135 import OpencvDemo135
+# from OpencvDemo136 import OpencvDemo136
+# from OpencvDemo137 import OpencvDemo137
+# from OpencvDemo138 import OpencvDemo138
+# from OpencvDemo139 import OpencvDemo139
+# from OpencvDemo140 import OpencvDemo140
 
 def main(argv):
     argc = len(argv)
@@ -495,6 +515,66 @@ def main(argv):
     elif demoId == 120:
         # 二维码检测与识别
         ret = OpencvDemo120()
+    elif demoId == 121:
+        # OpenCV DNN 获取导入模型各层信息
+        ret = OpencvDemo121()
+    # elif demoId == 122:
+    #     # OpenCV DNN 实现图像分类
+    #     ret = OpencvDemo122()
+    # elif demoId == 123:
+    #     # OpenCV DNN 为模型运行设置目标设备与计算后台
+    #     ret = OpencvDemo123()
+    # elif demoId == 124:
+    #     # OpenCV DNN 基于SSD实现对象检测
+    #     ret = OpencvDemo124()
+    # elif demoId == 125:
+    #     # OpenCV DNN 基于SSD实现实时视频检测
+    #     ret = OpencvDemo125()
+    # elif demoId == 126:
+    #     # OpenCV DNN 基于残差网络的人脸检测
+    #     ret = OpencvDemo126()
+    # elif demoId == 127:
+    #     # OpenCV DNN 基于残差网络的视频人脸检测
+    #     ret = OpencvDemo127()
+    # elif demoId == 128:
+    #     # OpenCV DNN 直接调用TensorFlow的导出模型
+    #     ret = OpencvDemo128()
+    # elif demoId == 129:
+    #     # OpenCV DNN 调用OpenPose模型实现姿态评估
+    #     ret = OpencvDemo129()
+    # elif demoId == 130:
+    #     # OpenCV DNN 支持YOLO对象检测网络运行
+    #     ret = OpencvDemo130()
+    # elif demoId == 131:
+    #     # OpenCV DNN 支持YOLOv3-tiny版本实时对象检测
+    #     ret = OpencvDemo131()
+    # elif demoId == 132:
+    #     # OpenCV DNN 单张与多张图像的推断
+    #     ret = OpencvDemo132()
+    # elif demoId == 133:
+    #     # OpenCV DNN 图像彩色化模型使用
+    #     ret = OpencvDemo133()
+    # elif demoId == 134:
+    #     # OpenCV DNN ENet实现图像分割
+    #     ret = OpencvDemo134()
+    # elif demoId == 135:
+    #     # OpenCV DNN 实时快速的图像风格迁移
+    #     ret = OpencvDemo135()
+    # elif demoId == 136:
+    #     # OpenCV DNN 解析网络输出结果
+    #     ret = OpencvDemo136()
+    # elif demoId == 137:
+    #     # OpenCV DNN 实现性别与年龄预测
+    #     ret = OpencvDemo137()
+    # elif demoId == 138:
+    #     # OpenCV DNN 使用OpenVINO加速
+    #     ret = OpencvDemo138()
+    # elif demoId == 139:
+    #     # 案例：识别0～9印刷体数字—Part1
+    #     ret = OpencvDemo139()
+    # elif demoId == 140:
+    #     # 案例：识别0～9印刷体数字—Part2
+    #     ret = OpencvDemo140()
     else:
         print("The argument is invalid!")
         return -1
